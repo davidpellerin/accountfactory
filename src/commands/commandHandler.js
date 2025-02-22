def6ad2e3876e52b4d27d5c9ebab8403ba2b4256
David Pellerin <david@davidpellerin.com>
2025-02-21 20:54:52 -0500

    fix for launching bin/accountfactory

--- src/commands/commandHandler.js
+++ src/commands/commandHandler.js
@@ -1,4 +1,3 @@
-import { logger } from '../utils/logger.js';
 import { STSService } from '../clients/stsService.js';
 import { OrganizationsService } from '../clients/organizationsService.js';
 import { configManager } from '../utils/config.js';
@@ -7,21 +6,25 @@ import { createInterface } from 'readline';
 import { IAMService } from '../clients/iamService.js';
 import { SetupProfilesService } from './setupProfilesService.js';
 import { SecretsManagerService } from '../clients/secretsManagerService.js';
+import { Logger } from "../utils/logger.js";
+
+const logger = new Logger();
 
 export class CommandHandler {
-  constructor(organizationsClient, iamClient, stsClient, secretsManagerClient) {
+  constructor(organizationsClient, iamClient, stsClient, secretsManagerClient, injectedLogger = logger) {
     this.organizationsClient = organizationsClient;
     this.iamClient = iamClient;
     this.stsClient = stsClient;
     this.secretsManagerClient = secretsManagerClient;
     this.configManager = configManager;
     this.generateSkeletonService = generateSkeletonService;
-    logger.debug('CommandHandler initialized with dependencies');
-    this.stsService = new STSService(this.stsClient);
-    this.organizationsService = new OrganizationsService(this.organizationsClient);
-    this.iamService = new IAMService(this.iamClient);
-    this.setupProfilesService = new SetupProfilesService(this.stsClient, this.secretsManagerClient);
-    this.secretsManagerService = new SecretsManagerService(this.secretsManagerClient);
+    this.logger = injectedLogger;
+    this.logger.debug('CommandHandler initialized with dependencies');
+    this.stsService = new STSService(this.stsClient, this.logger);
+    this.organizationsService = new OrganizationsService(this.organizationsClient, this.logger);
+    this.iamService = new IAMService(this.iamClient, this.secretsManagerClient, this.stsClient, this.logger);
+    this.setupProfilesService = new SetupProfilesService(this.stsClient, this.secretsManagerClient, this.logger);
+    this.secretsManagerService = new SecretsManagerService(this.secretsManagerClient, this.logger);
   }
 
   async confirm(message) {
@@ -40,46 +43,46 @@ export class CommandHandler {
 
   async handleListAccounts() {
     try {
-      logger.debug('Listing accounts');
+      this.logger.debug('Listing accounts');
       await this.stsService.getCallerIdentity();
       const accountList = await this.organizationsService.listOrganizationsAccounts();
       if (accountList && accountList.length > 0) {
         // eslint-disable-next-line no-console
         console.table(accountList);
       } else {
-        logger.info('No accounts found in AWS Organizations');
+        this.logger.info('No accounts found in AWS Organizations');
       }
     } catch (error) {
-      logger.error(`Command failed: ${error.message}`);
+      this.logger.error(`Command failed: ${error.message}`);
       process.exit(1);
     }
   }
 
   async handleListAccountsWithCredentials() {
     try {
-      logger.debug('Listing accounts');
+      this.logger.debug('Listing accounts');
       await this.stsService.getCallerIdentity();
       const accountList = await this.organizationsService.listOrganizationsAccounts();
       for (const account of accountList) {
         const password = await this.secretsManagerService.getExistingCredentials(account.Id, 'deploy');
-        logger.info(`${account.Id} - ${account.Email} - ${account.Status}`);
-        logger.info(password);
+        this.logger.info(`${account.Id} - ${account.Email} - ${account.Status}`);
+        this.logger.info(password);
       }
     } catch (error) {
-      logger.error(`Command failed: ${error.message}`);
+      this.logger.error(`Command failed: ${error.message}`);
       process.exit(1);
     }
   }
 
   async handleGenerateSkeleton() {
-    logger.debug('Generating skeleton');
+    this.logger.debug('Generating skeleton');
     const skeleton = await this.generateSkeletonService.generateSkeleton();
-    logger.info(skeleton);
+    this.logger.info(skeleton);
   }
 
   async handleCreateAccounts(options) {
 
-    logger.debug('Creating accounts');
+    this.logger.debug('Creating accounts');
 
     try {
       if (options.skipConfirmation === false) {
@@ -91,7 +94,7 @@ export class CommandHandler {
       const accountFactoryConfig = await this.configManager.readAccountFactoryConfig();
 
       if (accountFactoryConfig.accounts.length === 0) {
-        logger.error('No accounts found in accountfactory.json');
+        this.logger.error('No accounts found in accountfactory.json');
         process.exit(1);
       }
 
@@ -104,12 +107,12 @@ export class CommandHandler {
         );
 
         if (accountId) {
-          logger.info(`Account ${environmentConfig.email} created with ID ${accountId}`);
+          this.logger.info(`Account ${environmentConfig.email} created with ID ${accountId}`);
           await this.iamService.createIAMUser(accountId, options.username);
         }
       }
     } catch (error) {
-      logger.error(`Command failed: ${error.message}`);
+      this.logger.error(`Command failed: ${error.message}`);
       process.exit(1);
     }
   }
@@ -121,16 +124,11 @@ export class CommandHandler {
       const config = await this.configManager.readAccountFactoryConfig();
 
       for (const accountConfig of config.accounts) {
-        logger.info(`Setting up profiles for account ${accountConfig.email}`);
-
-        // accountName = 'Shared Services'
-        // email = 'owlsignalsshared@owlalerts.xyz'
-        // profileName = 'owlsignals-shared'
-
+        this.logger.info(`Setting up profiles for account ${accountConfig.email}`);
         await this.setupProfilesService.setupAwsProfile(accountConfig, liveAccountList, options);
       }
     } catch (error) {
-      logger.error(`Command failed: ${error.message}`);
+      this.logger.error(`Command failed: ${error.message}`);
       process.exit(1);
     }
   }
